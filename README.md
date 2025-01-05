e428e5d3d324e907184671972eeae4f16d699d6e
Ed Sweeney <ed@onextent.com>
2025-01-04 20:20:50 -0800

    2phase

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+create cluster
+=============
 
 ```
 tofu init
@@ -5,9 +7,16 @@ tofu init
 echo 'do_token = "your-digitalocean-api-token"' > terraform.tfvars
 echo 'letsencrypt_email = "you@yourdomain.com"' >> terraform.tfvars
 
+tofu plan -target=local_file.kubeconfig
+tofu apply -target=local_file.kubeconfig -auto-approve
 tofu plan
-
-tofu apply -target=local_file.kubeconfig
-tofu apply
+tofu apply -auto-approve
 ```
 
+delete cluster
+=============
+
+```
+kubectl --kubeconfig kubeconfig.yaml delete storageclass do-block-storage
+tofu destroy
+```
---
+++ k8s-resources.tf
@@ -0,0 +1,74 @@
+provider "kubernetes" {
+  config_path = "${path.module}/kubeconfig.yaml"
+}
+
+resource "local_file" "kubeconfig" {
+  content  = digitalocean_kubernetes_cluster.host_cluster.kube_config[0].raw_config
+  filename = "${path.module}/kubeconfig.yaml"
+}
+
+resource "kubernetes_storage_class" "do_block_storage" {
+  metadata {
+    name = "do-block-storage"
+  }
+
+  storage_provisioner = "dobs.csi.digitalocean.com"
+
+  depends_on = [local_file.kubeconfig]
+}
+
+provider "helm" {
+  kubernetes {
+    config_path = "${path.module}/kubeconfig.yaml"
+  }
+}
+
+resource "helm_release" "nginx_ingress" {
+  name       = "ingress-nginx"
+  repository = "https://kubernetes.github.io/ingress-nginx"
+  chart      = "ingress-nginx"
+  version    = "4.10.1"  # Use the latest stable version
+  namespace  = "ingress-nginx"
+
+  create_namespace = true
+}
+
+resource "helm_release" "cert_manager" {
+  name       = "cert-manager"
+  repository = "https://charts.jetstack.io"
+  chart      = "cert-manager"
+  version    = "1.13.0"  # Use the latest stable version
+  namespace  = "cert-manager"
+
+  create_namespace = true
+  set {
+    name  = "installCRDs"
+    value = "true"
+  }
+}
+
+resource "null_resource" "letsencrypt_issuer" {
+  provisioner "local-exec" {
+    command = <<EOT
+      cat > /tmp/letsencrypt-prod.yaml <<EOF
+      apiVersion: cert-manager.io/v1
+      kind: ClusterIssuer
+      metadata:
+        name: letsencrypt-prod
+      spec:
+        acme:
+          server: https://acme-v02.api.letsencrypt.org/directory
+          email: "${var.letsencrypt_email}"
+          privateKeySecretRef:
+            name: letsencrypt-prod
+          solvers:
+          - http01:
+              ingress:
+                class: nginx
+      EOF
+      kubectl apply -f /tmp/letsencrypt-prod.yaml
+    EOT
+  }
+
+  depends_on = [helm_release.cert_manager, local_file.kubeconfig]
+}
